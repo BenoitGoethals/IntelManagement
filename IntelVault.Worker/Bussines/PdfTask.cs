bc745a204442c314141a10bf7b86e86e6698b411
benoit <benoit.goethals@gmail.com>
2024-01-13 14:54:27 +0100

    init pdfscrapper

--- IntelVault.Worker/Bussines/PdfTask.cs
+++ IntelVault.Worker/Bussines/PdfTask.cs
@@ -1,6 +1,26 @@
-﻿namespace IntelVault.Worker.Bussines;
+﻿using IntelVault.ApplicationCore.Interfaces;
+using IntelVault.ApplicationCore.Model;
+using Quartz;
 
-public class PdfTask
+namespace IntelVault.Worker.Bussines;
+
+
+public class PdfTask : RequestJob, IJob
 {
-    
+
+    private readonly ILogger? _logger;
+    private PoolRequests? _poolRequests;
+    private IIntelService<IntelDocument>? _intelService;
+
+    public PdfTask(ILogger? logger, PoolRequests? poolRequests, IIntelService<IntelDocument>? intelService)
+    {
+        _logger = logger;
+        _poolRequests = poolRequests;
+        _intelService = intelService;
+    }
+
+    public override Task Execute(IJobExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
 }
