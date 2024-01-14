8c6e9dd97da5e2d23200d8882d9d24396c3c69d1
benoit <benoit.goethals@gmail.com>
2024-01-14 19:55:36 +0100

    update

--- IntelVault.Worker/Bussines/PdfTask.cs
+++ IntelVault.Worker/Bussines/PdfTask.cs
@@ -1,6 +1,11 @@
 ﻿using IntelVault.ApplicationCore.Interfaces;
 using IntelVault.ApplicationCore.Model;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf;
 using Quartz;
+using PdfSharp.Pdf.Content;
+using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace IntelVault.Worker.Bussines;
 
@@ -8,19 +13,61 @@ namespace IntelVault.Worker.Bussines;
 public class PdfTask : RequestJob, IJob
 {
 
-    private readonly ILogger? _logger;
+    private readonly ILogger<PdfTask>? _logger;
     private PoolRequests? _poolRequests;
     private IIntelService<IntelDocument>? _intelService;
+    private FileSystemWatcher watcher;
+    private string folderPath = "c:/temp";
 
-    public PdfTask(ILogger? logger, PoolRequests? poolRequests, IIntelService<IntelDocument>? intelService)
+
+    public PdfTask(ILogger<PdfTask>? logger, PoolRequests? poolRequests, IIntelService<IntelDocument>? intelService)
     {
         _logger = logger;
         _poolRequests = poolRequests;
         _intelService = intelService;
+
     }
 
-    public override Task Execute(IJobExecutionContext context)
+    public override async Task Execute(IJobExecutionContext context)
     {
-        throw new NotImplementedException();
+        await Task.Run(() =>
+        {
+            var files = Directory.GetFiles(folderPath);
+            foreach (var file in files)
+            {
+                string ret = SearchPdfFile(file);
+                
+            }
+        });
+      
     }
+
+
+
+
+    private string SearchPdfFile(string filePath)
+    {
+        try
+        {
+            using var doc = PdfReader.Open(filePath, PdfDocumentOpenMode.ReadOnly);
+            StringBuilder ta = new StringBuilder();
+            using (PdfSharpTextExtractor.Extractor extractor = new PdfSharpTextExtractor.Extractor(doc))
+            {
+                foreach (PdfPage page in doc.Pages)
+                {
+                    extractor.ExtractText(page, ta);
+                }
+
+            }
+            return ta.ToString();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing PDF file: {e.FullPath}. Error: {ex.Message}");
+        }
+    }
+
+
+
+
 }
